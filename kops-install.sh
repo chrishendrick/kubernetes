08d9119e3f5bbd3e9c737cca80c5d8596bec7d55
Chris Hendrick <chris.hendrick@gmail.com>
2018-09-06 10:08:50 -0400

    Update kops-install.sh
    v0.2

--- kops-install.sh
+++ kops-install.sh
@@ -1,3 +1,12 @@
+# install kops
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+# install python
+sudo apt-get install python3
+python --version
+
+# install AWS cli
+pip install awscli --upgrade --user
+aws --version
